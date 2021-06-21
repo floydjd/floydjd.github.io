ef94e7de2094203e9da0969fc1f5f18bfb72ba8a
Jordan Floyd <floyd.d.jordan@gmail.com>
2021-06-20 21:50:32 -0400

    Update styling of title

--- app/src/App/HomePage/JumboText.tsx
+++ app/src/App/HomePage/JumboText.tsx
@@ -1,6 +1,7 @@
 import { styled, Box } from '@material-ui/core';
 
 const JumboText = styled(Box)(({ theme }) => ({
+  textAlign: 'center',
   fontSize: theme.typography.h2.fontSize,
   letterSpacing: theme.spacing(1),
 }));
