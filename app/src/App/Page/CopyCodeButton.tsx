600d9d3450b641bae7fcc6cc70905e666270b7d4
Jordan Floyd <floyd.d.jordan@gmail.com>
2022-11-16 16:24:45 -0500

    Add copy button to code snippets

---
+++ app/src/App/Page/CopyCodeButton.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { ClipboardDocumentIcon, CheckIcon } from "@heroicons/react/24/solid";
+
+const CHECKBOX_TIMEOUT_SECONDS = 2;
+
+export interface CopyCodeButtonProps {
+  children: any;
+}
+
+export const CopyCodeButton: React.FC<CopyCodeButtonProps> = ({ children }) => {
+  const [copying, setCopying] = useState(false);
+
+  useEffect(() => {
+    if (copying) {
+      setTimeout(() => setCopying(false), CHECKBOX_TIMEOUT_SECONDS * 1000);
+    }
+  }, [copying, setCopying]);
+
+  const handleClick = () => {
+    setCopying(true);
+    navigator.clipboard.writeText(children[0].props.children[0]);
+  };
+
+  return (
+    <div className="code-copy-btn w-4 mt-2">
+      {copying ? <CheckIcon/> : <ClipboardDocumentIcon onClick={handleClick} color={"#c8c8c8"}/>}
+    </div>
+  );
+};
--- app/src/App/Page/Markdown.tsx
+++ app/src/App/Page/Markdown.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
 import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
 import typescript from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
 import scss from "react-syntax-highlighter/dist/cjs/languages/prism/scss";
@@ -10,6 +11,7 @@ import markdown from "react-syntax-highlighter/dist/cjs/languages/prism/markdown
 import json from "react-syntax-highlighter/dist/cjs/languages/prism/json";
 import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import rangeParser from "parse-numeric-range";
+import { CopyCodeButton } from "./CopyCodeButton";
 
 SyntaxHighlighter.registerLanguage("tsx", tsx);
 SyntaxHighlighter.registerLanguage("typescript", typescript);
@@ -23,53 +25,64 @@ export interface MarkdownProps {
 }
 
 export const Markdown: React.FC<MarkdownProps> = ({ children }) => {
-  const syntaxTheme = oneDark;
-
-  const MarkdownComponents: object = {
-    code({ node, className, children: rawText }: any) {
-      const match = /language-(\w+)/.exec(className || "");
-      const hasMeta = node?.data?.meta;
-
-      const applyHighlights: object = (hightlights: number) => {
-        if (hasMeta) {
-          const RE = /{([\d,-]+)}/;
-          const metadata = node.data.meta?.replace(/\s/g, "");
-          const strlineNumbers = RE.test(metadata)
-            ? RE.exec(metadata)?.at(1) || "0"
-            : "0";
-          const highlightLines = rangeParser(strlineNumbers);
-          const highlight = highlightLines;
-          const data = highlight.includes(hightlights)
-            ? "highlight"
-            : null;
-          return { data };
-        } else {
-          return {};
-        }
-      };
-
-      // Workaround for extra line bug https://github.com/react-syntax-highlighter/react-syntax-highlighter/issues/443
-      const text = (rawText || []).map((t: string) => t.replace(new RegExp("\\n$"), ""));
-
-      return match ? (
-        <SyntaxHighlighter
-          style={syntaxTheme}
-          language={match[1]}
-          PreTag="div"
-          className="codeStyle"
-          showLineNumbers={true}
-          wrapLines={hasMeta ? true : false}
-          useInlineStyles={true}
-          lineProps={applyHighlights}
-          children={text}
-        />
-      ) : (
-        <code className={className} children={text} />
-      );
-    },
-  };
-
   return (
-    <ReactMarkdown components={MarkdownComponents} remarkPlugins={[remarkGfm]}>{children}</ReactMarkdown>
+    <ReactMarkdown
+      rehypePlugins={[rehypeRaw]}
+      remarkPlugins={[remarkGfm]}
+      components={{
+        pre: (props: any) => (
+          <pre className="blog-pre">
+            <CopyCodeButton>{props.children}</CopyCodeButton>
+            {props.children}
+          </pre>
+        ),
+        code({ node, className, children: rawText }: any) {
+          const match = /language-(\w+)/.exec(className || "");
+          const hasMeta = node?.data?.meta;
+    
+          const applyHighlights: object = (hightlights: number) => {
+            if (hasMeta) {
+              const RE = /{([\d,-]+)}/;
+              const metadata = node.data.meta?.replace(/\s/g, "");
+              const strlineNumbers = RE.test(metadata)
+                ? RE.exec(metadata)?.at(1) || "0"
+                : "0";
+              const highlightLines = rangeParser(strlineNumbers);
+              const highlight = highlightLines;
+              const data = highlight.includes(hightlights)
+                ? "highlight"
+                : null;
+              return { data };
+            } else {
+              return {};
+            }
+          };
+    
+          // Workaround for extra line bug https://github.com/react-syntax-highlighter/react-syntax-highlighter/issues/443
+          const text = (rawText || []).map((t: string) => t.replace(new RegExp("\\n$"), ""));
+    
+          return match ? (
+            <SyntaxHighlighter
+              style={oneDark}
+              language={match[1]}
+              PreTag="div"
+              className="codeStyle"
+              showLineNumbers={true}
+              wrapLines={!!hasMeta}
+              useInlineStyles={true}
+              lineProps={applyHighlights}
+            >
+              {text}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className}>
+              {text}
+            </code>
+          );
+        },
+      }}
+    >
+        {children}
+    </ReactMarkdown>
   );
 };
